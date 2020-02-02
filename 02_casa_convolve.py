813df0851c8b1c697498bfc9478b2c93d2eb157a
IanHeywood <ian.heywood@gmail.com>
2020-02-02 14:00:06 +0000

    CASA script for convolution

---
+++ 02_casa_convolve.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# ian.heywood@physics.ox.ac.uk
+
+
+import config as cfg
+import glob
+import numpy
+import os
+import sys
+
+
+
+def main():
+
+
+	opfolder = cfg.opfolder
+	oplabel = cfg.oplabel
+	conv_bmaj = cfg.conv_bmaj
+	conv_bmin = cfg.conv_bmin
+	conv_bpa = cfg.conv_bpa
+
+
+	CWD = os.getcwd()
+	opfolder = CWD+'/'+opfolder.rstrip('/')+'/'
+
+
+	fitslist = glob.glob(opfolder.rstrip('/')+'/'+oplabel+'*fits')
+
+
+	for infits in fitslist:
+
+		opimg = infits.replace('.fits','_conv.img')
+		opfits = infits.replace('.fits','_conv.fits')
+
+		ia.open(infits)
+		ia.convolve2d(outfile=opimg,major=conv_bmaj,minor=conv_bmin,pa=conv_bpa)
+		exportfits(imagename=opimg,fitsimage=opfits)
+		os.system('rm -r '+opimg)
