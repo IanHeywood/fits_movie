30321a6abc0c60300909ccbdbd8228314a71ef88
IanHeywood <ian.heywood@gmail.com>
2020-02-02 15:13:55 +0000

    Make movie frames

--- 03_make_movie.py
+++ 03_make_movie.py
@@ -30,7 +30,12 @@ def main():
 	print fitslist
 
 	for infits in fitslist:
+
 		oppng = infits.replace('.fits','.png')
+		oppng_label = infits.replace('.fits','_stamp.png')
+		mjd = infits.split('_')[-1].split('.')[0].replace('p','.')
+		t = Time(float(mjd), format='mjd', scale='utc')
+		date = t.iso.split(' ')[0]
 
 		syscall = mViewer_exec+' '
 		syscall += '-ct '+str(cmap)+' '
@@ -41,6 +46,14 @@ def main():
 
 		os.system(syscall)
 
+		syscall = 'convert-im6 '+oppng+' '
+		syscall += "-stroke '#000C' -strokewidth 2 "
+		syscall += '-annotate 0 '+date+' '
+		syscall += '-stroke none -fill white '
+		syscall += '-annotate 0 '+date+' '
+		syscall += oppng_label
+
+		os.system(syscall)
 
 
 
