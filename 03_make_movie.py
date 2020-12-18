dfeb47ce986db285a95bb6d35cee768597617c8e
IanHeywood <IanHeywood@users.noreply.github.com>
2020-12-18 18:22:58 +0000

    Added time to GIF timestamps

--- 03_make_movie.py
+++ 03_make_movie.py
@@ -41,8 +41,8 @@ def main():
 
 		mjd = infits.split('_')[-2].split('.')[0].replace('p','.')
 		t = Time(float(mjd), format='mjd', scale='utc')
-		date = t.iso.split(' ')[0]
-
+		date = t.iso.replace(' ','_')
+		
 		syscall = mViewer_exec+' '
 		syscall += '-ct '+str(cmap)+' '
 		syscall += '-gray '+infits+' '
